9e724536240cb8840b0890f731a5c21539c30396
Ralf S. Engelschall <rse@engelschall.com>
2022-01-02 11:47:31 +0100

    simplify

--- streamDump.js
+++ streamDump.js
@@ -11,16 +11,14 @@ require('fs')
   .pipe(ebmlDecoder)
   .on('data', (tag) => {
       const str = tag.dump()
-      if (str.match(/^<\//)) {
+      if (str.match(/^<\//))
           indent--
-      }
       let prefix = ""
       for (let i = 0; i < indent; i++)
           prefix += "    "
       dump += prefix + str + "\n"
-      if (str.match(/^<(?:[^/]|[^/].*[^/])>$/)) {
+      if (str.match(/^<(?:[^/]|[^/].*[^/])>$/))
           indent++
-      }
   })
   .on('finish', () => {
       console.log(dump)
